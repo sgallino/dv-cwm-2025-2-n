37ce782a3976eab6a3cfea1f3116f1fb1bf69752
Santiago Gallino <gallino.santi@gmail.com>
2025-11-27 21:56:11 -0300

    Mueve las claves de Supabase al [.env]

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -3,7 +3,7 @@
 // sus funciones o métodos.
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://qhlbtrezdywgyfxjmeqt.supabase.co";
-const SUPABASE_KEY = "sb_publishable_qxo5mylK-UhrSCjyByqktQ_gO_DoJGj";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
